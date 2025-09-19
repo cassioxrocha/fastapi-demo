7362c9212024dd5b721bce1f0723c8ef93dc9d81
cxrocha <cxrocha@hotmail.com>
2025-09-19 15:31:14 -0300

    Novamente

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Oi, testando no VSCode"}
+    return {"message": "Testando Novamente!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
